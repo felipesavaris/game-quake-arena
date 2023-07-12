9b7e10cc8fcd45b731f2c47c9a5b165cf1f620de
Felipe Savaris <fesavaris@gmail.com>
2023-07-12 17:55:29 -0300

    Add parser script functionality

---
+++ scripts/parser/parser.py
@@ -0,0 +1,71 @@
+import json
+
+from utils import add_players, update_players_kills, increment_total_kills
+from models import Game
+
+
+data_objects = []
+
+
+def _stores_running_data(game: Game) -> list[dict]:
+    data = vars(game)
+
+    if data.get('game'):
+        key = data['game']
+        del data['game']
+
+        game_data = {
+            key: data,
+        }
+
+        data_objects.append(game_data)
+
+    return data_objects
+
+
+def _save_games(games: list[dict]) -> None:
+    with open('games_results.json', 'w') as database:
+        json.dump(games, database)
+
+
+def _parser_game_event_actions(log_file):
+    game_number = 0
+    game = None
+
+    for log in log_file:
+        if 'InitGame' in log:
+            game_number += 1
+            game = Game(number=game_number)
+
+        if 'Kill:' in log:
+            round_players = []
+
+            split_log = log.split(': ')
+            action = split_log[-1]
+
+            killer = action.split(' killed ')[0]
+            round_players.append(killer)
+
+            action_split = action.split(' killed ')
+            deceased = action_split[1].split(' by ')[0]
+            round_players.append(deceased)
+
+            add_players(players=round_players, game=game)
+
+            update_players_kills(players=round_players, game=game)
+
+            increment_total_kills(game=game)
+
+        if game:
+            all_games = _stores_running_data(game)
+            _save_games(games=all_games)
+
+
+if __name__ == '__main__':
+    print('Starting script!')
+
+    log_file = open('games.log', 'r')
+
+    _parser_game_event_actions(log_file)
+
+    print('Parser successfully executed! See the games_results.json file.')
---
+++ scripts/parser/utils.py
@@ -0,0 +1,44 @@
+from models import Game
+
+
+def _validate_player_exists(player: str, game: Game) -> bool:
+    if player in game.players:
+        return True
+    return
+
+
+def _add_player_kills_stucture(name: list[str], game: Game) -> None:
+    if name in game.players or name == '<world>':
+        return
+
+    game.players.append(name)
+    game.kills.update({f'{name}': 0})
+
+
+def _increment_kills(player: str, game: Game) -> None:
+    if (player != '<world>') or (player != player):
+        value = game.kills.get(player)
+        game.kills.update({player: int(value + 1)})
+
+
+def _decrement_kills(players: list[str], game: Game) -> None:
+    if players[0] == '<world>' or (players[0] == players[1]):
+        value = game.kills.get(players[1])
+        game.kills.update({players[1]: int(value - 1)})
+
+
+def add_players(players: list[str], game: Game) -> None:
+    for player in players:
+        player_exists = _validate_player_exists(player, game)
+
+        if not player_exists:
+            _add_player_kills_stucture(player, game)
+
+
+def update_players_kills(players: list[str], game: Game) -> None:
+    _increment_kills(player=players[0], game=game)
+    _decrement_kills(players=players, game=game)
+
+
+def increment_total_kills(game: Game) -> None:
+    game.total_kills += 1
